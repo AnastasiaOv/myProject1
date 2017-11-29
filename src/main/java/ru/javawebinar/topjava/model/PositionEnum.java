b12380e86f0a86edae2b5c46af861b13de041b39
anastasiaOv <sheep1995@mail.ru>
2017-11-29 20:15:43 +0300

    попытка исправить конструктор user

---
+++ src/main/java/ru/javawebinar/topjava/model/PositionEnum.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.topjava.model;
+
+/**
+ * Created by Анастасия on 29.11.2017.
+ */
+public enum PositionEnum {
+    POSITION_1,
+    POSITION_2,
+    POSITION_3;
+
+    public String getName() {
+        return name();
+    }
+}
